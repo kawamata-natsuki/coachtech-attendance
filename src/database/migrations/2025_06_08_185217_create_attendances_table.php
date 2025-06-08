dff5a0dda5ab0cfc1120846b5be62d546d9ed3c6
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-09 02:44:00 +0900

    feat：attendances テーブル作成

--- src/database/migrations/2025_06_08_185217_create_attendances_table.php
+++ src/database/migrations/2025_06_08_185217_create_attendances_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->unique(['user_id', 'work_date']);
 
             // 外部キー制約
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
         });
     }
 
