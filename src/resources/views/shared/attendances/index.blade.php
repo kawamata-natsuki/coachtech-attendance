5f87472b1466d9cbca97fb4e2d5b9a99647f110b
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-10 03:05:34 +0900

    feat: 勤怠一覧画面（共通ビューファイル）仮作成

---
+++ src/resources/views/shared/attendances/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/shared/attendances/index.css') }}">
+@endsection
+
+@section('content')
+<p>勤怠一覧画面（一般ユーザー）</p>
+@endsection
