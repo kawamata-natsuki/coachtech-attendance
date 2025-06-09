baa49a5ccbeda72e95205775a8ffa234fd7387ed
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-09 16:34:21 +0900

    feat: RegisterRequest作成

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -6,23 +6,34 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'max:255', 'string'],
+            'email' => ['required', 'email', 'unique:users,email', 'max:255', 'string'],
+            'password' => ['required', 'min:8', 'confirmed', 'string'],
+            'password_confirmation' => ['required']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'お名前を入力してください',
+            'name.max' => 'お名前は255文字以内で入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
+            'email.unique' => 'このメールアドレスはすでに登録されています',
+            'email.max' => 'メールアドレスは255文字以内で入力してください',
+            'password.required' => 'パスワードを入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.confirmed' => 'パスワードと一致しません',
+            'password_confirmation.required' => '確認用パスワードを入力してください',
         ];
     }
 }
