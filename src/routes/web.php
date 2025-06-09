8c975c7ed5df75438b112a75e96b9590360819b4
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-09 16:01:40 +0900

    feat：ログイン関連のルーティング設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
+use App\Http\Controllers\Admin\AttendanceController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ===============================
+// 認証関連ルート
+// ===============================
+
+// ログイン
+Route::get('/login', [LoginController::class, 'showLoginView'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+// ログアウト
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+// ===============================
+// ユーザールート（要ログイン）
+// ===============================
+
+// 出勤登録画面 
+Route::get('/attendance', [AttendanceController::class, 'record'])->name('attendance.record');
+
+// ===============================
+// 管理者ルート（要ログイン）
+// ===============================
