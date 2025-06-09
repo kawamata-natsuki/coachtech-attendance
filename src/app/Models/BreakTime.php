128ec341302be342e36957e781d977a4884c16e8
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-09 14:51:46 +0900

    feat: BreakTimeモデルに $casts を追加

--- src/app/Models/BreakTime.php
+++ src/app/Models/BreakTime.php
@@ -12,10 +12,15 @@ class BreakTime extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable =[
+    protected $fillable = [
         'attendance_id',
         'break_start',
         'break_end',
+    ];
+
+    protected $casts = [
+        'break_start' => 'datetime',
+        'break_end' => 'datetime',
     ];
 
     // リレーション
