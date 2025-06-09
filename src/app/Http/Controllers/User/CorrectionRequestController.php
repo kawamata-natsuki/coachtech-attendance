1fb8f5529d4b13c03b3e8854e648b91b3b004c66
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-10 01:39:17 +0900

    feat: CorrectionRequestController仮作成

--- src/app/Http/Controllers/User/CorrectionRequestController.php
+++ src/app/Http/Controllers/User/CorrectionRequestController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class CorrectionRequestController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('shared.correction-requests.index');
+    }
 }
