a2bd9eced89d28270a24e155486229c895bb2875
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-09 17:08:04 +0900

    feat: メール認証関連のルーティング設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AttendanceController;
+use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -17,13 +18,23 @@ Route::get('/', function () {
 // ユーザー登録
 Route::get('/register', [RegisterController::class, 'showRegisterView'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
-
 // ログイン
 Route::get('/login', [LoginController::class, 'showLoginView'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
-
 // ログアウト
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+// メール認証
+
+Route::prefix('email')->name('verification.')->middleware('auth')->group(function () {
+    Route::get('/verify', [EmailVerificationController::class, 'notice'])->name('notice');
+    Route::get('/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
+        ->middleware(['signed'])
+        ->name('verify');
+    Route::post('/verification-notification', [EmailVerificationController::class, 'resend'])
+        ->name('send');
+    Route::get('/check', [EmailVerificationController::class, 'check'])
+        ->name('check');
+});
 
 // ===============================
 // ユーザールート（要ログイン）
