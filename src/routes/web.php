1b8f226a3927356f7ab86ca2bbb0284a09a58912
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-10 01:34:15 +0900

    feat：ルーティング設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,29 +1,30 @@
 <?php
 
-use App\Http\Controllers\Admin\AttendanceController;
+use App\Http\Controllers\Admin\AttendanceController as AdminAttendanceController;
+use App\Http\Controllers\User\AttendanceController as UserAttendanceController;
+use App\Http\Controllers\Admin\StaffController;
 use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Admin\CorrectionRequestController as AdminCorrectionRequestController;
+use App\Http\Controllers\User\CorrectionRequestController as UserCorrectionRequestController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// 確認！パス名だぶりのとこ！
 
 // ===============================
-// 認証関連ルート
+// 認証ルート（要ログイン）
 // ===============================
 
-// ユーザー登録
 Route::get('/register', [RegisterController::class, 'showRegisterView'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
-// ログイン
-Route::get('/login', [LoginController::class, 'showLoginView'])->name('login');
+
+Route::get('/login', [LoginController::class, 'showLoginView'])->name('user.login');
 Route::post('/login', [LoginController::class, 'store']);
-// ログアウト
+Route::get('/admin/login', [LoginController::class, 'showLoginView'])->name('admin.login');
+Route::post('admin/login', [LoginController::class, 'store']);
+
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-// メール認証
 
 Route::prefix('email')->name('verification.')->middleware('auth')->group(function () {
     Route::get('/verify', [EmailVerificationController::class, 'notice'])->name('notice');
@@ -39,21 +40,44 @@ Route::prefix('email')->name('verification.')->middleware('auth')->group(functio
 // ===============================
 // ユーザールート（要ログイン）
 // ===============================
+Route::middleware(['auth', 'verified'])
+    ->name('user.')
+    ->group(function () {
+        Route::get('/attendance', [UserAttendanceController::class, 'record'])->name('attendance.record');
+        Route::post('/attendance', [UserAttendanceController::class, 'store'])->name('attendance.store');
 
-// 出勤登録画面 
-Route::get('/attendance', [AttendanceController::class, 'record'])->name('attendance.record');
-Route::post('/attemdamce', [AttendanceController::class, 'store'])->name('attendance.store');
-// 勤怠一覧画面
-Route::get('/attendance/list', [AttendanceController::class, 'index'])->name('user.attendance.index');
+        Route::get('/attendance/list', [UserAttendanceController::class, 'index'])->name('attendance.index');
+
+        Route::get('/attendance/{id}', [UserAttendanceController::class, 'show'])->name('attendance.show');
+        Route::post('/attendance/{id}', [UserAttendanceController::class, 'update'])->name('attendance.update');
+
+        Route::get('/stamp_correction_request/list', [UserCorrectionRequestController::class, 'index'])->name('correction-request.index');
+    });
 
 // ===============================
 // 管理者ルート（要ログイン）
 // ===============================
-// 勤怠一覧画面
-Route::get('/admin/attendance/list', [AttendanceController::class, 'index'])->name('admin.attendance.index');
+Route::middleware(['auth', 'verified', 'admin'])
+    ->name('admin.')
+    ->group(function () {
+        // 勤怠一覧画面
+        Route::get('/admin/attendance/list', [AdminAttendanceController::class, 'index'])->name('attendance.index');
 
-// ===============================
-// 共通ルート（要ログイン）
-// ===============================
-// 勤怠詳細画面
-Route::get('/attendance/{id}', [AttendanceController::class, 'show'])->name('attendance.show');
+        // 勤怠詳細画面
+        Route::get('/attendance/{id}', [AdminAttendanceController::class, 'show'])->name('attendance.show');
+        Route::post('/attendance/{id}', [AdminAttendanceController::class, 'update'])->name('attendance.update');
+
+        // スタッフ一覧画面
+        Route::get('/admin/staff/list', [StaffController::class, 'index'])->name('staff.index');
+
+        // スタッフ別勤怠一覧画面
+        Route::get('/admin/attendance/staff/{id}', [AdminAttendanceController::class, 'staff'])->name('attendance.staff');
+        Route::get('/admin/attendance/staff/{id}/export', [AdminAttendanceController::class, 'exportCsv'])->name('attendance.export');
+
+        // 申請一覧画面
+        Route::get('/stamp_correction_request/list', [AdminCorrectionRequestController::class, 'index'])->name('correction-request.index');
+
+        // 修正申請承認画面
+        Route::get('/stamp_correction_request/approve/{attendance_correct_request}', [AdminCorrectionRequestController::class, 'show'])->name('correction-request.show');
+        Route::post('/stamp_correction_request/approve/{attendance_correct_request}', [AdminCorrectionRequestController::class, 'approve'])->name('correction-request.approve');
+    });
