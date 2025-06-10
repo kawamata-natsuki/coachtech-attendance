f57f36af8c90818d1d0c9118e26ca70f33b0dfc4
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-10 15:45:27 +0900

    refactor: ログインエラーのキーをemailからloginに変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,7 +61,7 @@ class LoginController extends Controller
         }
 
         return back()->withErrors([
-            'email' => 'ログイン情報が登録されていません',
+            'login' => 'ログイン情報が登録されていません',
         ])->onlyInput('email');
     }
 }
