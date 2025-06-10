ce653575a3e591b05e8db7749ed8b2a3b88ebeb4
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-11 01:58:53 +0900

    chore: Googlefontの読み込み

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/layouts/common.css') }}">
   @yield('css')
   <title>coachtech 勤怠管理アプリ</title>
@@ -20,8 +21,10 @@
       </div>
 
       @auth
+      @if(Auth::user()->hasVerifiedEmail())
       @includeWhen(Auth::user()->isAdmin(), 'components.nav.admin')
       @includeUnless(Auth::user()->isAdmin(), 'components.nav.user')
+      @endif
       @endauth
     </nav>
   </header>
