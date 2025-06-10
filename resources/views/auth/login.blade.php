93b561921223e4caaf1ef198bc99342623484532
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-10 16:13:34 +0900

    feat: ログイン画面のビューファイルとCSSファイル作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,54 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
 @endsection
 
-@section('title', '会員登録')
-
 @section('content')
-<div class="register-page">
-  <div class="register-page__container">
-    <h1 class="register-page__heading content__heading">
-      会員登録
+<div class="login-page">
+  <div class="login-page__container">
+    <h1 class="login-page__heading content__heading">
+      ログイン
     </h1>
 
-    <div class="register-page__content">
-      <form class="register-page__form" action="/register" method="post" novalidate>
+    <div class="login-page__content">
+      <form class="login-page__form" action="/login" method="post" novalidate>
         @csrf
 
-        <div class="register-page__form-section">
-          <!-- ユーザー名 -->
-          <div class="register-page__form-group">
-            <label class="register-page__label form__label" for="name">ユーザー名</label>
-            <input class="form__input register-page__input" type="text" name="name" id="name" value="{{ old('name') }}"
-              placeholder="例：山田　太郎">
-            <x-error.validation-error field="name" class="error-message {{ $errors->has('name') ? 'has-error' : 'no-error' }}" />
-          </div>
+        <!-- ログイン失敗時のエラー -->
+        <div class="login-page__form-group">
+          <x-error.message field="name" />
+        </div>
 
+        <div class="login-page__form-section">
           <!-- メールアドレス -->
-          <div class="register-page__form-group">
-            <label class="register-page__label form__label" for="email">メールアドレス</label>
-            <input class="form__input register-page__input" type="email" name="email" id="email"
-              value="{{ old('email') }}" placeholder="例：user@example.com">
-            <x-error.validation-error field="email"
-              class="error-message {{ $errors->has('email') ? 'has-error' : 'no-error' }}" />
+          <div class="login-page__form-group form-group">
+            <label class="login-page__label form__label" for="email">メールアドレス</label>
+            <input class="login-page__input form__input" type="email" name="email" id="email" value="{{ old('email') }}"
+              placeholder="例：user@example.com">
+            <x-error.message field="email" />
           </div>
 
           <!-- パスワード -->
-          <div class="register-page__form-group">
-            <label class="register-page__label form__label" for="password">パスワード</label>
-            <input class="form__input register-page__input" type="password" name="password" id="password"
+          <div class="login-page__form-group form-group">
+            <label class="login-page__label form__label" for="password">パスワード</label>
+            <input class="login-page__input form__input" type="password" name="password" id="password"
               placeholder="8文字以上のパスワードを入力">
-            <x-error.validation-error field="password"
-              class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}"
-              :excludeMessage="'パスワードと一致しません'" />
+            <x-error.message field="password" />
           </div>
+        </div>
 
-          <!-- 送信ボタン -->
-          <div class="register-page__button">
-            <button class="button--solid-red register-page__button-submit" type="submit">登録する</button>
-          </div>
+        <!-- 送信ボタン -->
+        <div class="login-page__button">
+          <button class="auth-button" type="submit">ログインする</button>
+        </div>
       </form>
-    </div>
 
-    <!-- リンク -->
-    <div class="register-page__link">
-      <a href="/login" class="register-page__link--login">ログインはこちら</a>
+      <!-- リンク -->
+      <div class="login-page__link">
+        <a href="/register" class="login-page__link--register">会員登録はこちら</a>
+      </div>
     </div>
   </div>
-</div>
 </div>
 @endsection
