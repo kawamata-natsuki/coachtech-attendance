0751c41fe28c8e1c685872ecf122e0adbc5b1662
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-13 03:22:24 +0900

    style: 月ナビゲーションのコンポーネントファイル・共通レイアウト作成

---
+++ resources/views/components/month/nav.blade.php
@@ -0,0 +1,23 @@
+<!-- 月ナビゲーション -->
+<div class="month-nav">
+  <div class="month-nav__navigation">
+    <a class="month-nav__link" href="{{ route('user.attendances.index', ['month' => $currentMonth->copy()->subMonth()->format('Y-m')]) }}">
+      <span class="month-nav__link-inner"><img src="{{ asset('images/left-arrow.svg') }}" alt="前月" class="icon-arrow">前月
+      </span>
+    </a>
+  </div>
+
+  <div class="month-nav__current-month">
+    <span class="month-nav-label">
+      <img src="{{ asset('images/calendar.svg') }}" alt="カレンダーアイコン" class="icon-calendar">
+      {{ $currentMonth->format('Y/m') }}
+    </span>
+  </div>
+
+  <div class="month-nav__navigation">
+    <a class="month-nav__link" href="{{ route('user.attendances.index', ['month' => $currentMonth->copy()->addMonth()->format('Y-m')]) }}">
+      <span class="month-nav__link-inner">翌月<img src="{{ asset('images/right-arrow.svg') }}" alt="翌月" class="icon-arrow">
+      </span>
+    </a>
+  </div>
+</div>
