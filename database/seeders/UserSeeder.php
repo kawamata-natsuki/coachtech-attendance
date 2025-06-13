8727b87b39f0a800a51d04c4860b625f8e2b95f8
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-13 12:37:39 +0900

    add: joining_dateのダミーデータ追加

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,12 +11,36 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $users = [
-            ['name' => '西 怜奈', 'email' => 'reina.n@coachtech.com'],
-            ['name' => '山田 太郎', 'email' => 'taro.y@coachtech.com'],
-            ['name' => '増田 一世', 'email' => 'issei.m@coachtech.com'],
-            ['name' => '山田 敬吉', 'email' => 'keikichi.y@coachtech.com'],
-            ['name' => '秋田 朋美', 'email' => 'tomomi.a@coachtech.com'],
-            ['name' => '中西 教夫', 'email' => 'norio.n@coachtech.com'],
+            [
+                'name' => '西 怜奈',
+                'email' => 'reina.n@coachtech.com',
+                'joining_date' => '2025-04-01',
+            ],
+            [
+                'name' => '山田 太郎',
+                'email' => 'taro.y@coachtech.com',
+                'joining_date' => '2025-04-01',
+            ],
+            [
+                'name' => '増田 一世',
+                'email' => 'issei.m@coachtech.com',
+                'joining_date' => '2025-04-01',
+            ],
+            [
+                'name' => '山田 敬吉',
+                'email' => 'keikichi.y@coachtech.com',
+                'joining_date' => '2025-04-01',
+            ],
+            [
+                'name' => '秋田 朋美',
+                'email' => 'tomomi.a@coachtech.com',
+                'joining_date' => '2025-04-01',
+            ],
+            [
+                'name' => '中西 教夫',
+                'email' => 'norio.n@coachtech.com',
+                'joining_date' => '2025-04-01',
+            ],
         ];
 
         foreach ($users as $user) {
@@ -25,6 +49,7 @@ class UserSeeder extends Seeder
                 'email' => $user['email'],
                 'password' => Hash::make('12345678'),
                 'email_verified_at' => now(),
+                'joining_date' => $user['joining_date'],
             ]);
         }
 
