9709401a7a2c485a35c1ec4d53e7a6993ccbb682
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-16 02:38:05 +0900

    feat: 勤怠修正リクエストの一覧表示にユーザー情報を追加

--- app/Http/Controllers/User/CorrectionRequestController.php
+++ app/Http/Controllers/User/CorrectionRequestController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\CorrectionRequest;
 use Illuminate\Http\Request;
 
 class CorrectionRequestController extends Controller
 {
     public function index()
     {
-        return view('shared.correction-requests.index');
+        $correctionRequests = CorrectionRequest::with('user')
+            ->where('user_id', auth()->id())
+            ->get();
+
+        return view('shared.correction-requests.index', compact('correctionRequests'));
     }
 }
