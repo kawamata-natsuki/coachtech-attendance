e267517cf7a5d7547775f1829d329bf5a06f5604
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-16 02:39:14 +0900

    feat: 会員登録フォームのエラーメッセージにpreserve属性を追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,7 +23,7 @@
             <label class="register-page__label form__label" for="name">名前</label>
             <input class="form__input register-page__input" type="text" name="name" id="name" value="{{ old('name') }}"
               placeholder="例：山田　太郎">
-            <x-error.message field="name" />
+            <x-error.message field="name" preserve />
           </div>
 
           <!-- メールアドレス -->
@@ -31,7 +31,7 @@
             <label class="register-page__label form__label" for="email">メールアドレス</label>
             <input class="form__input register-page__input" type="email" name="email" id="email"
               value="{{ old('email') }}" placeholder="例：user@example.com">
-            <x-error.message field="email" />
+            <x-error.message field="email" preserve />
           </div>
 
           <!-- パスワード -->
@@ -39,7 +39,7 @@
             <label class="register-page__label form__label" for="password">パスワード</label>
             <input class="form__input register-page__input" type="password" name="password" id="password"
               placeholder="8文字以上のパスワードを入力">
-            <x-error.message field="password" />
+            <x-error.message field="password" preserve />
           </div>
 
           <!-- 確認用パスワード -->
@@ -47,7 +47,7 @@
             <label class="register-page__label form__label" for="password_confirmation">パスワード確認</label>
             <input class="form__input register-page__input" type="password" name="password_confirmation"
               id="password_confirmation" placeholder="もう一度パスワードを入力">
-            <x-error.message field="password_confirmation" />
+            <x-error.message field="password_confirmation" preserve />
           </div>
         </div>
 
