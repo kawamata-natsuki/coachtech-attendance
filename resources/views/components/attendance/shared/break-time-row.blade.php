94e49ebbd8e4bacfc2e1453ca60a7b08567eb279
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-21 20:00:18 +0900

    feat: 休憩時刻の入力方式をテキストボックスからセレクトボックスに変更

--- resources/views/components/attendance/shared/break-time-row.blade.php
+++ resources/views/components/attendance/shared/break-time-row.blade.php
@@ -7,41 +7,55 @@
 'isNew' => false,
 ])
 
+<!-- 入力の初期値（休憩開始時刻・休憩終了時刻）を「時」と「分」に分割 -->
+<!-- 例："12:00"-> ["12", "00"] -->
+<!-- isNew = True （新しく追加された空の休憩行） -->
+<!-- isNew = False （既存の休憩データあり） -->
+@php
+if ($isNew) {
+$start = [null, null];
+$end = [null, null];
+} else {
+$start = explode(':', old("requested_breaks.$index.requested_break_start", optional($breakStart)->format('H:i')));
+$end = explode(':', old("requested_breaks.$index.requested_break_end", optional($breakEnd)->format('H:i')));
+}
+@endphp
+
 <tr class="attendance-show-page__table-row">
-  <th class="attendance-show-page__table-head">休憩{{ $index + 1 }}</th>
-  <td class="attendance-show-page__table-cell">
-    <div class="attendance-show-page__form-group">
+  <th class="attendance-show-page__table-head">
+    休憩{{ $index + 1 }}
+  </th>
 
-      {{-- 入力フォーム --}}
-      <div class="attendance-show-page__time-wrapper {{ $disabled ? 'attendance-show-page__time-wrapper--disabled' : '' }}">
-        <input class="attendance-show-page__time-input"
-          type="text"
-          name="requested_breaks[{{ $index }}][requested_break_start]"
-          value="{{ old("requested_breaks.$index.requested_break_start", optional($breakStart)->format('H:i')) }}"
-          pattern="\d{2}:\d{2}"
-          inputmode="numeric"
-          {{ $disabled ? 'disabled' : '' }}>
+  <td class="attendance-show-page__table-cell time-select-cell">
+    <div @class([ 'time-range-wrapper' , 'time-range-wrapper--disabled'=> $disabled,])>
 
-        <span class="attendance-show-page__separator">～</span>
+      <!-- 休憩開始時刻セレクトボックス -->
+      <x-attendance.shared.time-select
+        name="requested_breaks[{{ $index }}][requested_break_start]"
+        :selectedHour="data_get($start, 0)"
+        :selectedMinute="data_get($start, 1)"
+        :disabled="$disabled" />
 
-        <input class="attendance-show-page__time-input"
-          type="text"
-          name="requested_breaks[{{ $index }}][requested_break_end]"
-          value="{{ old("requested_breaks.$index.requested_break_end", optional($breakEnd)->format('H:i')) }}"
-          pattern="\d{2}:\d{2}"
-          inputmode="numeric"
-          {{ $disabled ? 'disabled' : '' }}>
+      <span class="time-range-separator">～</span>
 
-        @if (!$disabled && !$isNew && $breakId)
-        <input type="hidden" name="requested_breaks[{{ $index }}][break_time_id]" value="{{ $breakId }}">
-        @endif
-      </div>
+      <!-- 休憩終了時刻セレクトボックス -->
+      <x-attendance.shared.time-select
+        name="requested_breaks[{{ $index }}][requested_break_end]"
+        :selectedHour="data_get($end, 0)"
+        :selectedMinute="data_get($end, 1)"
+        :disabled="$disabled" />
 
-      {{-- エラーメッセージ --}}
-      <div class="attendance-show-page__error-wrapper">
-        <x-error.attendance-message :field="'requested_breaks.' . $index . '.requested_break_start'" />
-        <x-error.attendance-message :field="'requested_breaks.' . $index . '.requested_break_end'" />
-      </div>
+      <!-- 既存の休憩データを編集する際に break_time_id を送る -->
+      @if (!$disabled && !$isNew && $breakId)
+      <input type="hidden" name="requested_breaks[{{ $index }}][break_time_id]" value="{{ $breakId }}">
+      @endif
+    </div>
+
+    <div class="time-select__error-wrapper">
+      <x-error.attendance-message :field="'requested_breaks.' . $index . '.requested_break_start'" />
+      <x-error.attendance-message :field="'requested_breaks.' . $index . '.requested_break_end'" />
+      <x-error.attendance-message :field="'break_time_logic_' . $index" />
+    </div>
 
     </div>
   </td>
