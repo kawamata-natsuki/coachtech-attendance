495f016eef32445418480bc277f3793e4e3c65b0
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-21 19:29:22 +0900

    feat: 出勤・退勤時刻の入力方式をテキストボックスからセレクトボックスに変更

--- resources/views/components/attendance/shared/work-time-row.blade.php
+++ resources/views/components/attendance/shared/work-time-row.blade.php
@@ -9,23 +9,37 @@
     出勤・退勤
   </th>
 
-  <td class="attendance-show-page__table-cell">
-    <div
-      class="attendance-show-page__time-wrapper {{ $disabled ? 'attendance-show-page__time-wrapper--disabled' : '' }}">
-      <input class="attendance-show-page__time-input" type="text" name="requested_clock_in"
-        value="{{ old('requested_clock_in', $clockIn) }}" pattern="^([01]\d|2[0-3]):[0-5]\d$" inputmode="numeric" {{
-        $disabled ? 'disabled' : '' }}>
+  <td class="attendance-show-page__table-cell time-select-cell">
+    <div @class([ 'time-range-wrapper' , 'time-range-wrapper--disabled'=> $disabled,])>
 
-      <span class="attendance-show-page__separator">～</span>
+      <!-- 入力の初期値（出勤・退勤）を「時」と「分」に分割 -->
+      <!-- 例："09:00"-> ["09", "00"] -->
+      @php
+      $clockInParts = explode(':', old('requested_clock_in', $clockIn ?? ''));
+      $clockOutParts = explode(':', old('requested_clock_out', $clockOut ?? ''));
+      @endphp
 
-      <input class="attendance-show-page__time-input" type="text" name="requested_clock_out"
-        value="{{ old('requested_clock_out', $clockOut) }}" pattern="^([01]\d|2[0-3]):[0-5]\d$" inputmode="numeric" {{
-        $disabled ? 'disabled' : '' }}>
+      <!-- 出勤時刻セレクトボックス -->
+      <x-attendance.shared.time-select
+        name="requested_clock_in"
+        :selectedHour="data_get($clockInParts, 0)"
+        :selectedMinute="data_get($clockInParts, 1)"
+        :disabled="$disabled" />
+
+      <span class="time-range-separator">～</span>
+
+      <!-- 退勤時刻セレクトボックス -->
+      <x-attendance.shared.time-select
+        name="requested_clock_out"
+        :selectedHour="data_get($clockOutParts, 0)"
+        :selectedMinute="data_get($clockOutParts, 1)"
+        :disabled="$disabled" />
     </div>
 
-    <div class="attendance-show-page__error-wrapper">
+    <div class="time-select__error-wrapper">
       <x-error.attendance-message field="requested_clock_in" />
       <x-error.attendance-message field="requested_clock_out" />
+      <x-error.attendance-message field="work_time_logic" />
     </div>
 
     </div>
