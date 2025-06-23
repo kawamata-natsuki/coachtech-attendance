a86ade9b58ba05b8ebc252177b0657c86730fd47
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-23 17:01:26 +0900

    refactor: CorrecttionRequestController を作成、申請一覧画面の表示処理を改追加

--- app/Http/Controllers/User/CorrectionRequestController.php
+++ app/Http/Controllers/User/CorrectionRequestController.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Enums\ApprovalStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AttendanceCorrectionRequest;
 use App\Models\Attendance;
+use App\Models\CorrectionRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 
 class CorrectionRequestController extends Controller
 {
+    // 申請一覧画面の表示処理
     public function index(Request $request)
     {
-        return view('shared.correction-requests.index');
+        $status = $request->query('status', 'pending');
+
+        $query = CorrectionRequest::with('user')
+            ->where('user_id', auth()->id());
+
+        if ($status === 'pending') {
+            $query->where('approval_status', ApprovalStatus::PENDING);
+        } elseif ($status === 'approved') {
+            $query->where('approval_status', ApprovalStatus::APPROVED);
+        }
+
+        $correctionRequests = $query->get();
+
+        return view('shared.correction-requests.index', [
+            'status' => $status,
+            'correctionRequests' => $correctionRequests,
+        ]);
     }
 }
