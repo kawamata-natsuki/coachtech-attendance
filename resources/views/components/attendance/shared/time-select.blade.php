507134c3a9e541380ffc15802037c1e882d11f73
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-23 09:53:20 +0900

    refactor: 時間入力用セレクトボックスのコメント整理とdisabled属性の削除

--- resources/views/components/attendance/shared/time-select.blade.php
+++ resources/views/components/attendance/shared/time-select.blade.php
@@ -1,25 +1,26 @@
+{{-- 出退勤・休憩時間などの時間入力用セレクトボックス --}}
+
 @props([
 'name',
 'selectedHour' => null,
 'selectedMinute' => null,
-'disabled' => false,
 ])
 
 <div class="time-select-wrapper">
-  <!-- 時間（Hour） -->
+  {{-- 時間（Hour） --}}
   <select
     class="time-select-wrapper__select"
-    name="{{ $name }}[hour]"
-    {{ $disabled ? 'disabled' : '' }}>
+    name="{{ $name }}[hour]">
 
     <option value="" {{ $selectedHour === null ? 'selected' : '' }}>
       --
     </option>
 
+    {{-- 00〜23 を2桁文字列に変換してコレクション生成 --}}
     @php
     $hours = collect(range(0, 23))->map(fn($h) => str_pad($h, 2, '0', STR_PAD_LEFT));
     @endphp
-    <!-- 選択肢00～23を生成 -->
+
     @foreach ($hours as $h)
     <option value="{{ $h }}" {{ $h == $selectedHour ? 'selected' : '' }}>
       {{ $h }}
@@ -30,20 +31,20 @@
 
   <span class="time-separator">:</span>
 
-  <!-- 分（Minute） -->
+  {{-- 分（Minute） --}}
   <select
     class="time-select-wrapper__select"
-    name="{{ $name }}[minute]"
-    {{ $disabled ? 'disabled' : '' }}>
+    name="{{ $name }}[minute]">
 
     <option value="" {{ $selectedMinute === null ? 'selected' : '' }}>
       --
     </option>
 
+    {{-- 00〜59 を2桁文字列に変換してコレクション生成 --}}
     @php
     $minutes = collect(range(0, 59))->map(fn($m) => str_pad($m, 2, '0', STR_PAD_LEFT));
     @endphp
-    <!-- 選択肢00～59を生成 -->
+
     @foreach ($minutes as $m)
     <option value="{{ $m }}" {{ $m == $selectedMinute ? 'selected' : '' }}>
       {{ $m }}
