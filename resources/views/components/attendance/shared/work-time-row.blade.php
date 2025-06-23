9d938bddd6b571533eb6c6e93d7265fe8b88e8dc
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-23 10:17:55 +0900

    refactor: 出退勤時間入力用セレクトボックスからdisabled属性を削除し、コードを整理

--- resources/views/components/attendance/shared/work-time-row.blade.php
+++ resources/views/components/attendance/shared/work-time-row.blade.php
@@ -1,7 +1,6 @@
 @props([
 'clockIn' => null,
 'clockOut' => null,
-'disabled' => false,
 ])
 
 <tr class="attendance-show-page__table-row">
@@ -11,42 +10,40 @@
 
   <td class="attendance-show-page__table-cell--time-select">
     <div class="time-range-wrapper">
+      <div class="time-inputs">
 
-      <!-- 入力の初期値（出勤・退勤）を「時」と「分」に分割 -->
-      <!-- 例："09:00"-> ["09", "00"] -->
-      @php
-      $clockInHour = old('requested_clock_in.hour', data_get(explode(':', $clockIn), 0));
-      $clockInMinute = old('requested_clock_in.minute', data_get(explode(':', $clockIn), 1));
+        {{-- 入力の初期値（出勤・退勤）を「時」と「分」に分割  --}}
+        @php
+        $clockInHour = old('requested_clock_in.hour', data_get(explode(':', $clockIn), 0));
+        $clockInMinute = old('requested_clock_in.minute', data_get(explode(':', $clockIn), 1));
 
-      $clockOutHour = old('requested_clock_out.hour', data_get(explode(':', $clockOut), 0));
-      $clockOutMinute = old('requested_clock_out.minute', data_get(explode(':', $clockOut), 1));
-      @endphp
+        {{-- バリデーションエラー後の再表示はold()の値を優先 --}}
+        $clockOutHour = old('requested_clock_out.hour', data_get(explode(':', $clockOut), 0));
+        $clockOutMinute = old('requested_clock_out.minute', data_get(explode(':', $clockOut), 1));
+        @endphp
 
-      <!-- 出勤時刻セレクトボックス -->
-      <div class="time-block">
-        <x-attendance.shared.time-select
-          name="requested_clock_in"
-          :selectedHour="$clockInHour"
-          :selectedMinute="$clockInMinute"
-          :disabled="$disabled" />
-      </div>
+        {{-- 出勤時刻セレクトボックス  --}}
+        <div class="time-block">
+          <x-attendance.shared.time-select
+            name="requested_clock_in"
+            :selectedHour="$clockInHour"
+            :selectedMinute="$clockInMinute" />
+        </div>
 
-      <span class="time-range-separator">～</span>
+        <span class="time-range-separator">～</span>
 
-      <!-- 退勤時刻セレクトボックス -->
-      <div class="time-block">
-        <x-attendance.shared.time-select
-          name="requested_clock_out"
-          :selectedHour="$clockOutHour"
-          :selectedMinute="$clockOutMinute"
-          :disabled="$disabled" />
+        {{-- 退勤時刻セレクトボックス  --}}
+        <div class="time-block">
+          <x-attendance.shared.time-select
+            name="requested_clock_out"
+            :selectedHour="$clockOutHour"
+            :selectedMinute="$clockOutMinute" />
+        </div>
       </div>
 
       <div class="time-select__error-wrapper">
         <x-error.attendance-message field="requested_clock_in" />
         <x-error.attendance-message field="requested_clock_out" />
-        <x-error.attendance-message field="work_time_logic" />
-        <x-error.attendance-message field="requested_clock_out.hour" />
       </div>
 
     </div>
