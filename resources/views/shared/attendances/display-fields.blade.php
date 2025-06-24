c158ab1768947fe1b22c12c6adf6d567a24a5b87
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-24 23:36:38 +0900

    fix: 休憩の表示を修正し、インデックスを追加

--- resources/views/shared/attendances/display-fields.blade.php
+++ resources/views/shared/attendances/display-fields.blade.php
@@ -15,19 +15,16 @@
 $breaks = $correctionRequest?->correctionBreakTimes ?? $attendance->breakTimes;
 @endphp
 
-@foreach ($breaks as $break)
+@foreach ($correctionRequest->correctionBreakTimes as $index => $break)
+@continue(is_null($break->requested_break_start) && is_null($break->requested_break_end))
 <tr class="attendance-show-page__table-row">
   <th class="attendance-show-page__table-head">
-    休憩
+    休憩{{ $index + 1 }}
   </th>
   <td class="attendance-show-page__text--time">
-    <span class="time-display-block">
-      {{ $break->requested_break_start?->format('H:i') ?? $break->break_start?->format('H:i') ?? '--:--' }}
-    </span>
-    <span class="time-display-separator">～</span>
-    <span class="time-display-block">
-      {{ $break->requested_break_end?->format('H:i') ?? $break->break_end?->format('H:i') ?? '--:--' }}
-    </span>
+    {{ optional($break->requested_break_start)->format('H:i') ?? '--:--' }}
+    ～
+    {{ optional($break->requested_break_end)->format('H:i') ?? '--:--' }}
   </td>
 </tr>
 @endforeach
