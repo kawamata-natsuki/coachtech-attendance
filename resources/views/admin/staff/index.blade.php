8c1c352a23d3bb630a55ba4482753949b17f96ce
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-24 10:34:44 +0900

    feat: スタッフ一覧ページを新規作成し、スタイルを追加

---
+++ resources/views/admin/staff/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/admin/staff/index.css') }}">
+@endsection
+
+@section('title', 'スタッフ一覧')
+
+@section('content')
+<div class="staff-index-page">
+  <div class="staff-index-page__container">
+    <h1 class="staff-index-page__heading content__heading">
+      <span class="staff-index-page__heading-text">
+        スタッフ一覧
+      </span>
+    </h1>
+
+    <!-- 名前・メールアドレス・月次勤怠　詳細ボタン -->
+    <table class="staff-index-page__table">
+
+      <thead>
+        <tr>
+          <th class="staff-index-page__table-head">
+            名前
+          </th>
+          <th class="staff-index-page__table-head">
+            メールアドレス
+          </th>
+          <th class="staff-index-page__table-head">
+            月次勤怠
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        @foreach ($users as $user)
+        <tr>
+          <td class="staff-index-page__table-cell">
+            {{ $user->name }}
+          </td>
+          <td class="staff-index-page__table-cell">
+            {{ $user->email }}
+          </td>
+          <td class="staff-index-page__table-cell">
+            <a class="staff-index-page__table-link" href="{{ route('admin.attendances.staff', ['id' => $user->id]) }}">
+              詳細
+            </a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+@endsection
