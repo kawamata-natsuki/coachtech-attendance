1ac117c747e3cb878e390e8201303f030ef482dd
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-25 13:14:07 +0900

    feat: 申請詳細表示の改善と管理者用ログインリンクの追加

--- resources/views/components/nav/admin.blade.php
+++ resources/views/components/nav/admin.blade.php
@@ -15,11 +15,17 @@
     </a>
   </li>
   <li>
+    @if(Auth::guard('admin')->check())
     <form id="logout-form" method="POST" action="{{ route('admin.logout') }}">
       @csrf
       <button class="header-menu__link" type="submit">
         ログアウト
       </button>
     </form>
+    @else
+    <a href="{{ route('admin.login') }}" class="header-menu__link">
+      ログイン
+    </a>
+    @endif
   </li>
 </ul>
