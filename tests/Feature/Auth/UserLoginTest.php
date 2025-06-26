85cb0a133643a3cb6b187bb17286ce88f1608d36
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-27 01:39:03 +0900

    test: ユーザーログインテストの修正（createUser()で事前にユーザー登録）

--- tests/Feature/Auth/UserLoginTest.php
+++ tests/Feature/Auth/UserLoginTest.php
@@ -4,22 +4,26 @@ namespace Tests\Feature\Auth;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\TestHelpers\AuthTestHelper;
 
 class UserLoginTest extends TestCase
 {
     use RefreshDatabase;
+    use AuthTestHelper;
 
     /**
      * メールアドレスが未入力の場合、バリデーションメッセージが表示される
      */
     public function test_login_fails_when_email_is_missing(): void
     {
+        $user = $this->createUser();
+
         $response = $this->get('/login');
         $response->assertStatus(200);
 
         $response = $this->post('/login', [
             'email' => '',
-            'password' => 'pass1234'
+            'password' => 'guest123'
         ]);
 
         // 「メールアドレスを入力してください」というバリデーションメッセージが表示される
@@ -33,11 +37,13 @@ class UserLoginTest extends TestCase
      */
     public function test_login_fails_when_password_is_missing(): void
     {
+        $user = $this->createUser();
+
         $response = $this->get('/login');
         $response->assertStatus(200);
 
         $response = $this->post('/login', [
-            'email' => 'test@example.com',
+            'email' => 'guest@example.com',
             'password' => '',
         ]);
 
@@ -50,17 +56,17 @@ class UserLoginTest extends TestCase
     /**
      * 登録内容と一致しない場合、バリデーションメッセージが表示される
      */
-    public function test_login_fails_when_password_is_incorrect(): void
+    public function test_login_fails_when_user_credentials_do_not_match(): void
     {
+        $user = $this->createUser();
+
         $response = $this->get('/login');
         $response->assertStatus(200);
 
         $response = $this->post('/login', [
-            'email' => 'coachtech@example.com',
-            'password' => 'pass1234',
+            'email' => 'wrong@example.com',
+            'password' => 'guest123'
         ]);
-
-        dump(session('errors')->all());
 
         // 「ログイン情報が登録されていません」というバリデーションメッセージが表示される
         $response->assertSessionHasErrors([
