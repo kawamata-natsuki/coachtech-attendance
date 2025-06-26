79202d4d44f8c2f67b143670c4f9763b25b22b90
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-26 14:43:36 +0900

    style: headerのロゴリンクメソッドを削除

--- app/Helpers/LayoutHelper.php
+++ app/Helpers/LayoutHelper.php
@@ -7,23 +7,6 @@ use Illuminate\Support\Facades\Auth;
 // 	ロゴのリンク、ナビゲーションなど、共通レイアウトに関するビュー補助処理
 class LayoutHelper
 {
-  // headerのロゴリンククリック時の遷移先
-  public static function headerTopLink(): string
-  {
-    // 管理者ログインしている場合は管理者の勤怠一覧画面へ
-    if (Auth::guard('admin')->check()) {
-      return route('admin.attendances.index');
-    }
-
-    // ユーザーログインしている場合はユーザーの勤怠登録画面へ
-    if (Auth::guard('web')->check()) {
-      return route('user.attendances.record');
-    }
-
-    // 未ログインはログインページへリダイレクト
-    return route('login');
-  }
-
   // headerのナビゲーションメニューのビューを返す
   public static function headerNavView(): ?string
   {
