86ae731db149c5ca6df20edba4463107affc0aee
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-01 02:56:30 +0900

    chore: 不要なUserモデルのインポートを削除

--- tests/Feature/Admin/AttendanceDetailTest.php
+++ tests/Feature/Admin/AttendanceDetailTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Admin;
 
 use App\Models\Attendance;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestHelpers\AuthTestHelper;
 use Tests\TestCase;
