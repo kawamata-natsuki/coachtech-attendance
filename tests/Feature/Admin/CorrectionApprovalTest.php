dfa0d7e52d8ad744de40e71ea1ff0428a7aa797e
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-06-30 23:17:51 +0900

    rename: 管理者用のテストファイル名変更

--- tests/Feature/Admin/CorrectionApprovalTest.php
+++ tests/Feature/Admin/CorrectionApprovalTest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Tests\Feature\Correction;
+namespace Tests\Feature\Admin;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class AdminCorrectionApprovalTest extends TestCase
+class CorrectionApprovalTest extends TestCase
 {
     /**
      * A basic feature test example.
