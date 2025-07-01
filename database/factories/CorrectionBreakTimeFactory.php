05d1336b88619be61bc5ed7c486f375f2273614c
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-01 16:19:41 +0900

    refactor: CorrectionBreakTimeFactoryから correction_request_id と break_tume_id を削除

--- database/factories/CorrectionBreakTimeFactory.php
+++ database/factories/CorrectionBreakTimeFactory.php
@@ -13,12 +13,7 @@ class CorrectionBreakTimeFactory extends Factory
 
     public function definition(): array
     {
-        $correctionRequest = CorrectionRequest::factory()->create();
-        $breakTime = BreakTime::factory()->create(['attendance_id' => $correctionRequest->attendance_id]);
-
         return [
-            'correction_request_id' => $correctionRequest->id,
-            'break_time_id' => $breakTime->id,
             'requested_break_start' => now()->setTime(13, 0),
             'requested_break_end' => now()->setTime(14, 0),
             'original_break_start' => now()->setTime(12, 0),
