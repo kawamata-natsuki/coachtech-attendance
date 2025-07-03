644e9130c7027dd7f17ecf49d1643b688212d3aa
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-04 00:03:30 +0900

    refactor: スタッフ一覧ページのテーブル構造を改善し、可読性を向上

--- resources/views/admin/staff/index.blade.php
+++ resources/views/admin/staff/index.blade.php
@@ -16,40 +16,43 @@
     </h1>
 
     <!-- 名前・メールアドレス・月次勤怠　詳細ボタン -->
-    <table class="staff-index-page__table">
+    <div class="staff-index-page__table-wrapper">
+      <table class="staff-index-page__table">
+        <thead>
+          <tr>
+            <th class="staff-index-page__table-head">
+              名前
+            </th>
+            <th class="staff-index-page__table-head">
+              メールアドレス
+            </th>
+            <th class="staff-index-page__table-head">
+              月次勤怠
+            </th>
+          </tr>
+        </thead>
 
-      <thead>
-        <tr>
-          <th class="staff-index-page__table-head">
-            名前
-          </th>
-          <th class="staff-index-page__table-head">
-            メールアドレス
-          </th>
-          <th class="staff-index-page__table-head">
-            月次勤怠
-          </th>
-        </tr>
-      </thead>
+        <tbody>
+          @foreach ($users as $user)
+          <tr>
+            <td class="staff-index-page__table-cell">
+              {{ $user->name }}
+            </td>
 
-      <tbody>
-        @foreach ($users as $user)
-        <tr>
-          <td class="staff-index-page__table-cell">
-            {{ $user->name }}
-          </td>
-          <td class="staff-index-page__table-cell">
-            {{ $user->email }}
-          </td>
-          <td class="staff-index-page__table-cell">
-            <a class="staff-index-page__table-link" href="{{ route('admin.attendances.staff', ['id' => $user->id]) }}">
-              詳細
-            </a>
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
+            <td class="staff-index-page__table-cell">
+              {{ $user->email }}
+            </td>
+
+            <td class="staff-index-page__table-cell">
+              <a class="staff-index-page__table-link" href="{{ route('admin.attendances.staff', ['id' => $user->id]) }}">
+                詳細
+              </a>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
 @endsection
